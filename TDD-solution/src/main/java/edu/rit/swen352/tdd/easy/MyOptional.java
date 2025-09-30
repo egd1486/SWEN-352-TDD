151ffb729ef2842597e8a25a795649def2b2a5fd
bab8295 <bab8295@g.rit.edu>
2025-09-30 11:23:28 -0400

    MyOptional : 5 : assert .ofNullable() doesn't return null : create passing code

--- TDD-solution/src/main/java/edu/rit/swen352/tdd/easy/MyOptional.java
+++ TDD-solution/src/main/java/edu/rit/swen352/tdd/easy/MyOptional.java
@@ -41,8 +41,7 @@ public class MyOptional<T> {
     }
 
     static public <E> MyOptional<E> ofNullable(E value) {
-        assert false;
-        return null;
+        return new MyOptional<E>(null);
     }
 
     public T get() {
