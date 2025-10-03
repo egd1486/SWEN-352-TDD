223ba9b8ed795657bc7c8239dd1166726a1d4f5a
SamCollier75 <sjc8315@rit.edu>
2025-10-03 12:53:37 -0400

    bankAccount : 3 : assure negative cents throws an exception : create failing test

--- TDD-solution/src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
+++ TDD-solution/src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
@@ -4,7 +4,9 @@ package edu.rit.swen352.tdd.hard;
  * A Value Object for American monetary values with fixed integer
  * values for dollars and cents.
  */
-record Money(int dollars, int cents) {}
+record Money(int dollars, int cents) {
+    
+}
 
 /**
  * An American bank account that permits deposits and withdrawals.
--- TDD-solution/src/test/java/edu/rit/swen352/tdd/hard/BankAccountTest.java
+++ TDD-solution/src/test/java/edu/rit/swen352/tdd/hard/BankAccountTest.java
@@ -13,12 +13,25 @@ class BankAccountTest {
     private BankAccount CuT;
 
     @Test
-    public void moneyConstructorTest() {
+    @DisplayName("Money record can be constructed")
+    public void moneyConstructorTest(){
         assertNotNull(RuT = new Money(0, 0));
     }
 
     @Test
+    @DisplayName("BankAccount can be constructed")
     public void bankAccountConstructorTest(){
         assertNotNull(CuT = new BankAccount());
+    }
+
+    @Test
+    @DisplayName("Cents cannot be negative")
+    public void centsCannotBeNegativeTest(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            RuT = new Money(0, -1);
+        });
+        String expectedMessage = "Cents cannot be negative";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
