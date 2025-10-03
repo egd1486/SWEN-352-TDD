f45a2401abf8839131b17c07e6e11ff8ec95d26d
SamCollier75 <sjc8315@rit.edu>
2025-10-03 11:53:59 -0400

    bankAccount : 1 : construct a money record : create a passing test

--- TDD-solution/src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
+++ TDD-solution/src/main/java/edu/rit/swen352/tdd/hard/BankAccount.java
@@ -4,7 +4,7 @@ package edu.rit.swen352.tdd.hard;
  * A Value Object for American monetary values with fixed integer
  * values for dollars and cents.
  */
-// record Money() {}
+record Money() {}
 
 /**
  * An American bank account that permits deposits and withdrawals.
